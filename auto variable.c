70abe40168b2d03b8a6161f0b8842096b3ea7894
Dr Neha Sharma <nehasharma0110@gmail.com>
2025-01-17 10:57:34 +0530

    Add files via upload

---
+++ auto variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void function()
+{
+    int x = 10; // local variable (also automatic)
+    auto int y = 20; // automatic variable
+    printf("Auto Variable: %d", y);
+}
+int main()
+{
+
+    function();
+    return 0;
+}
